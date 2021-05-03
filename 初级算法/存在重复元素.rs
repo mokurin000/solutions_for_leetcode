ea55325a709916fe12fcf45fdc1300d3ab9fd45e
poly000 <34085039+poly000@users.noreply.github.com>
2021-05-04 06:34:25 +0800

    Update 存在重复元素.rs

--- 初级算法/存在重复元素.rs
+++ 初级算法/存在重复元素.rs
@@ -1,3 +1,19 @@
+/*
+
+给定一个整数数组，判断是否存在重复元素。
+
+如果存在一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+
+示例 3:
+
+输入: [1,1,1,3,3,4,3,2,4,2]
+输出: true
+
+https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/x248f5/
+
+*/
+
+
 impl Solution {
     pub fn contains_duplicate(mut nums: Vec<i32>) -> bool {
         nums.sort_unstable();
