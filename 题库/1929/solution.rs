b39d85e09e448f2585915e3d946cc65469ff5072
poly000 <1348292515@qq.com>
2022-01-02 10:38:47 +0800

    rename

--- 题库/1929/solution.rs
+++
@@ -1,8 +0,0 @@
-impl Solution {
-    pub fn get_concatenation(nums: Vec<i32>) -> Vec<i32> {
-        nums.into_iter()
-            .chain(nums.into_iter())
-            .map(|&n| n)
-            .collect()
-    }
-}
--- 题库/58/solution.rs
+++
@@ -1,9 +0,0 @@
-impl Solution {
-    pub fn length_of_last_word(s: String) -> i32 {
-        if let Some(re) = s.split_whitespace().last() {
-                re.len() as i32
-        } else {
-            0
-        }
-    }
-}
