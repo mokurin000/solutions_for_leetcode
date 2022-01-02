b938e12ebf0869d992d93bdaf42cac5f2c2ba221
poly000 <34085039+poly000@users.noreply.github.com>
2022-01-02 10:17:16 +0800

    Update solution.rs

--- 题库/172/solution.rs
+++ 题库/172/solution.rs
@@ -1,6 +1,6 @@
 impl Solution {
-    fn zeros(n: i32) -> i32 {
-        (1u32..).map(|i|n / 5u64.pow(i))
+    fn trailing_zeroes(n: i32) -> i32 {
+        (1u32..).map(|i|n / 5i32.pow(i))
             .take_while(|&r| r != 0)
             .sum()
     }
